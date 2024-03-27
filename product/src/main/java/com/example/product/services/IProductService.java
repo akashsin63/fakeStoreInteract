a4caccceb0e16bbf0c6df823f4bc4b1bc9870ba7
Akash <akashsingh54812@gmail.com>
2024-03-27 23:03:35 +0530

    service layer change

--- product/src/main/java/com/example/product/services/IProductService.java
+++ product/src/main/java/com/example/product/services/IProductService.java
@@ -1,7 +1,10 @@
 package com.example.product.services;
 
+import org.springframework.stereotype.Service;
+
 import com.example.product.models.Product;
 
+@Service
 public interface IProductService {
     public Product getSingleProduct(Long id);
 }
